5c85fa3fb357734a48a4ea5f3e360adba350273e
Henric Trotzig <henric.trotzig@gmail.com>
2018-10-18 22:50:03 +0200

    Remove cruft from happoExamples
    This was remains from when we had this in a demo project.

--- happoExamples.js
+++ happoExamples.js
@@ -1,15 +1,5 @@
 const styleElem = document.createElement('style');
-styleElem.innerHTML = HAPPO_DATA.css + `
-
-  @media (max-width: 1024px) and (min-width: 991px) {
-    #page-top.nc-consumer .home-header.us-header {
-        min-height: 630px;
-        background-position: -80px bottom;
-    }
-  }
-
-
-`;
+styleElem.innerHTML = HAPPO_DATA.css;
 document.head.appendChild(styleElem);
 
 module.exports = HAPPO_DATA.examples.map(({ component, html }) => {
